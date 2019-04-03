4617b4e9f245278ab1919271ebfc7fd1b204cdba
AliOgretmen <aliogretmen.ch@gmail.com>
2019-04-03 17:49:14 +0200

    Style Binding

--- src/app/components/class-binding/class-binding.component.ts
+++ src/app/components/class-binding/class-binding.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-class-binding',
   template: `<h3>{{name}}</h3>
-            <h3 class="text-success">Class Binding</h3>
-            <h3 [class]="styleSuccess">Class Binding</h3>
-            <h3 [class]="text-success" class="text-italic">Class Binding</h3>
-            <h3 [class.text-danger]="styleError">Class Binding</h3>
-            <h3 [ngClass]="styleClasses">Class Binding</h3>`,
+            <h3 class="text-success">Class Binding 1</h3>
+            <h3 [class]="styleSuccess">Class Binding 2</h3>
+            <h3 [class]="text-success" class="text-italic">Class Binding 3</h3>
+            <h3 [class.text-danger]="styleError">Class Binding 4</h3>
+            <h3 [ngClass]="styleClasses">Class Binding 5</h3>
+            <h3 [style.color]="'yellow'">Style Binding 1</h3>
+            <h3 [style.color]="styleError ? 'yellow' : 'orange'">Style Binding 2</h3>
+            <h3 [style.color]="blueColor"> Style Binding 3</h3>
+            <h3 [ngStyle]="titleStyles"> Style Binding 4</h3>`,
   styles: [`
      .text-success {
        color: green;
@@ -30,6 +34,11 @@ export class ClassBindingComponent implements OnInit {
     "text-success": !this.styleError,
     "text-danger": this.styleError,
     "text-italic": this.isItalic
+  };
+  public blueColor = "blue";
+  public titleStyles ={
+    color: "purple",
+    fontStyle: "italic"
   }
 
   constructor() { }
