37b06aacd23b85cc750baf07c2d7885ad91f1d9f
AliOgretmen <aliogretmen.ch@gmail.com>
2019-04-03 20:22:22 +0200

    ngIf

--- src/app/components/directives/directives.component.ts
+++ src/app/components/directives/directives.component.ts
@@ -2,10 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
-  templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.scss']
+  template: `
+      <h3 *ngIf="true">Directives 1</h3>
+      <h3 *ngIf="headerName">Directives 2</h3>
+      <h3 *ngIf="headerName else condition">Directives 3</h3>
+      <ng-template #condition>
+       <h3>Name is hidden</h3>
+      </ng-template>
+      <div *ngIf="headerName; then thenBlock; else elseBlock"></div>
+      <ng-template #thenBlock>
+       <h3>Then Block</h3>
+      </ng-template>
+      <ng-template #elseBlock>
+       <h3>Else Block</h3>
+      </ng-template>
+      
+      `,
+  styles: []
 })
 export class DirectivesComponent implements OnInit {
+
+  headerName = false;
 
   constructor() { }
 
